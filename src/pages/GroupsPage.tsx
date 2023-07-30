8dd92764c0081bf4419d96dd7b052ed2b47faa45
Abdul Rehman <ar618211@gmail.com>
2023-07-30 23:13:03 +0500

    group link

--- src/pages/GroupsPage.tsx
+++ src/pages/GroupsPage.tsx
@@ -79,10 +79,10 @@ function GroupsPage() {
             Join Group
           </button>
         </div>
-        <ul>
+        <ul className="w-full flex flex-col gap-2 items-center mt-4">
           {groups.map(group => (
             <li key={group.id}>
-              <Link to={`/group/${group.id}`}>
+              <Link to={`/group/${group.id}`} className="btn btn-neutral btn-md w-max">
                 {group.name}
               </Link>
             </li>
